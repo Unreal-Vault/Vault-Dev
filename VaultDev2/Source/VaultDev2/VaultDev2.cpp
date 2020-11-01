81797546777e70531f3be4799c2c85468d707ec9
Daniel Orchard <legan3d@gmail.com>
2020-11-01 18:31:01 +0000

    Refactoring github to only include plugin and not dev-project

--- VaultDev2/Source/VaultDev2/VaultDev2.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "VaultDev2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VaultDev2, "VaultDev2" );
--- VaultDev2/Source/VaultDev2/VaultDev2GameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "VaultDev2GameModeBase.h"
-
